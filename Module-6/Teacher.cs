87ea4f0d423e30d140a3db8050d8ee27c5e17cf2
Thomas <t.golda@yahoo.co.uk>
2017-03-19 22:10:41 +0000

    Create Teacher.cs

---
+++ Module-6/Teacher.cs
@@ -0,0 +1,34 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M6Homework
+{
+    class Teacher : Person
+    {
+
+        //properties
+        public Student Obj{get; set;}
+        public string Grade { get; set; }
+
+        //constructor
+        public Teacher(string FirstN, string LastN, string className)
+        {
+            this.Name = FirstN;
+            this.Lastname = LastN;
+            this.Class = className;
+          
+        }
+
+        // teacher's methods
+        public void GradeTest(Student student, int grade)
+        {
+            this.Obj = student;
+            this.Grade = grade.ToString();
+            Console.WriteLine("Teacher {0} {1} graded the {2} {3} as {4}", this.Name,this.Lastname,this.Obj.Name,this.Obj.Lastname,this.Grade);
+        }
+        
+    }
+}
