8ffc720a26ba4426549ccb64f4e9f3f5164ad487
Thomas <t.golda@yahoo.co.uk>
2017-03-19 22:11:41 +0000

    Create Uprogram.cs

---
+++ Module-6/Uprogram.cs
@@ -0,0 +1,35 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M6Homework
+{
+    class Uprogram
+    {
+
+        // variables
+        private Degree degree;
+        private string title;
+        
+
+        //properties
+        public string Title{ get { return title; }set { title = value; }}
+        public Degree Degree { get { return degree; } set {degree = value; } }
+        
+       
+        // constructor
+        public Uprogram(string title)
+        {
+            this.title = title;
+        }
+
+    // addUprogram method
+    public void addUprogram(Degree degree)
+    {
+        this.degree = degree;
+    }
+
+}
+}
